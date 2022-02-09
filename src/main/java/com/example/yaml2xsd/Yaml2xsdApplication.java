e6452e0876d2f926b5e089ea9a99495d829392d0
Ardeshir Ahouri <ardeshir.ahouri@gmail.com>
2022-02-09 14:51:03 +0330

    init commit

---
+++ src/main/java/com/example/yaml2xsd/Yaml2xsdApplication.java
@@ -0,0 +1,13 @@
+package com.example.yaml2xsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yaml2xsdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Yaml2xsdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yaml2xsd/Yaml2xsdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yaml2xsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yaml2xsdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
